3d55d8fa8476102e869ffb8be9805e594d46a505
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2016-07-13 13:47:12 +0100

    Add class: Slogans

---
+++ MonsterBrand/Classes/Slogans.swift
@@ -0,0 +1,21 @@
+//
+//  Slogans.swift
+//  Pods
+//
+//  Created by Krishna Kotecha on 13/07/2016.
+//
+//
+
+import Foundation
+
+public class Slogans {
+
+    public func brandTagLine() -> String {
+        return "Monsters. Any time. Any place."
+    }
+    
+    public func phoneTagLine() -> String {
+        return "Putting Monsters in your pocket."
+    }
+
+}
